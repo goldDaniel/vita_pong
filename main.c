794c2093a2a15e1a8330538c4cd68702ad3182ab
goldDaniel <gold.daniel@outlook.com>
2018-10-20 00:34:52 -0500

    removed unneeded code

--- main.c
+++ main.c
@@ -13,11 +13,6 @@
 
 #define PADDLE_SPEED 6;
 
-/*
- * Symbol of the image.png file
- */
-//extern unsigned char _binary_image_png_start;
-
 typedef struct RECTANGLE Rect;
 struct RECTANGLE
 {
@@ -48,20 +43,13 @@ float lerp(float a, float b, float t);
 int main()
 {
 	SceCtrlData pad;
-	vita2d_pgf *pgf;
 	vita2d_pvf *pvf;
 	float rad = 0.0f;
 
 	vita2d_init();
 	vita2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
 
-	pgf = vita2d_load_default_pgf();
 	pvf = vita2d_load_default_pvf();
-
-	/*
-	 * Load the statically compiled image.png file.
-	 */
-	//image = vita2d_load_PNG_buffer(&_binary_image_png_start);
 
 	memset(&pad, 0, sizeof(pad));
 
@@ -92,7 +80,7 @@ int main()
     {
 		sceCtrlPeekBufferPositive(0, &pad, 1);
 
-		if(pad.buttons & SCE_CTRL_START)
+        if(pad.buttons & SCE_CTRL_START)
 		{
             paused = 1;
         }	
@@ -224,7 +212,6 @@ int main()
 	 * then we can free the assets freely.
 	 */
 	vita2d_fini();
-	vita2d_free_pgf(pgf);
 	vita2d_free_pvf(pvf);
 
 	sceKernelExitProcess(0);
@@ -242,7 +229,7 @@ void clampToScreen(Rect* rect)
 int areRectanglesColliding(Rect rect1, Rect rect2)
 {
     int horizontalColliding = 0;
-    int verticalColliding = 0;
+    int verticalColliding =   0;
     if(rect1.x > rect2.x && rect1.x < rect2.x + rect2.width)
     {
         horizontalColliding = 1;
